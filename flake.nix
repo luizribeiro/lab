211a247fff2d1abe721d69415b59564a43fc32fc
Luiz Ribeiro <luizribeiro@gmail.com>
2026-03-20 15:27:21 -0400

    chore(pre-commit): enforce clippy warnings with consistent toolchain

--- flake.nix
+++ flake.nix
@@ -60,6 +60,16 @@
             src = ./.;
             hooks = {
               rustfmt.enable = true;
+              clippy = {
+                enable = true;
+                packageOverrides = {
+                  inherit (pkgs) cargo clippy;
+                };
+                settings = {
+                  denyWarnings = true;
+                  extraArgs = "--target-dir target/pre-commit";
+                };
+              };
               nixpkgs-fmt.enable = true;
               statix.enable = true;
               deadnix.enable = true;
