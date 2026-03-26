luizribeiro/capsa

feat(core): add network interface config and launch envelope to VmConfig

crates/core/src/config.rs
@@ -1,8 +1,17 @@
 use std::path::PathBuf;
 
+use anyhow::{bail, Result};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+/// Network interface configuration for a VM (user-facing).
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct NetworkInterfaceConfig {
+    /// MAC address (auto-generated if None).
+    #[serde(default)]
+    pub mac: Option<[u8; 6]>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct VmConfig {
     pub root: Option<PathBuf>,
     pub kernel: Option<PathBuf>,
@@ -11,4 +20,48 @@ pub struct VmConfig {
     pub vcpus: u8,
     pub memory_mib: u32,
     pub verbosity: u8,
+    /// Network interfaces. Empty = no networking (default).
+    #[serde(default)]
+    pub interfaces: Vec<NetworkInterfaceConfig>,
+}
+
+impl VmConfig {
+    pub fn validate(&self) -> Result<()> {
+        if self.interfaces.len() > 1 {
+            bail!("multiple network interfaces are not supported yet");
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{NetworkInterfaceConfig, VmConfig};
+
+    fn sample_vm_config() -> VmConfig {
+        VmConfig {
+            root: Some("/tmp/root".into()),
+            kernel: None,
+            initramfs: None,
+            kernel_cmdline: None,
+            vcpus: 1,
+            memory_mib: 512,
+            verbosity: 0,
+            interfaces: vec![],
+        }
+    }
+
+    #[test]
+    fn vm_config_validate_rejects_multiple_interfaces() {
+        let mut config = sample_vm_config();
+        config.interfaces = vec![
+            NetworkInterfaceConfig { mac: None },
+            NetworkInterfaceConfig { mac: None },
+        ];
+
+        let err = config.validate().expect_err("validation should fail");
+        assert!(err
+            .to_string()
+            .contains("multiple network interfaces are not supported yet"));
+    }
 }

crates/core/src/lib.rs
@@ -4,7 +4,8 @@ mod launcher;
 mod libkrun;
 mod runtime;
 
-pub use config::VmConfig;
+pub use config::{NetworkInterfaceConfig, VmConfig};
+pub use vmm_spec::{ResolvedNetworkInterface, VmmLaunchSpec};
 
 #[doc(hidden)]
 pub use runtime::start_vm;

luizribeiro/lab

crates/core/src/config.rs
@@ -1,8 +1,17 @@
 use std::path::PathBuf;
 
+use anyhow::{bail, Result};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+/// Network interface configuration for a VM (user-facing).
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct VmNetworkInterfaceConfig {
+    /// MAC address (auto-generated if None).
+    #[serde(default)]
+    pub mac: Option<[u8; 6]>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct VmConfig {
     pub root: Option<PathBuf>,
     pub kernel: Option<PathBuf>,
@@ -11,4 +20,48 @@ pub struct VmConfig {
     pub vcpus: u8,
     pub memory_mib: u32,
     pub verbosity: u8,
+    /// Network interfaces. Empty = no networking (default).
+    #[serde(default)]
+    pub interfaces: Vec<VmNetworkInterfaceConfig>,
+}
+
+impl VmConfig {
+    pub fn validate(&self) -> Result<()> {
+        if self.interfaces.len() > 1 {
+            bail!("multiple network interfaces are not supported yet");
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{VmConfig, VmNetworkInterfaceConfig};
+
+    fn sample_vm_config() -> VmConfig {
+        VmConfig {
+            root: Some("/tmp/root".into()),
+            kernel: None,
+            initramfs: None,
+            kernel_cmdline: None,
+            vcpus: 1,
+            memory_mib: 512,
+            verbosity: 0,
+            interfaces: vec![],
+        }
+    }
+
+    #[test]
+    fn vm_config_validate_rejects_multiple_interfaces() {
+        let mut config = sample_vm_config();
+        config.interfaces = vec![
+            VmNetworkInterfaceConfig { mac: None },
+            VmNetworkInterfaceConfig { mac: None },
+        ];
+
+        let err = config.validate().expect_err("validation should fail");
+        assert!(err
+            .to_string()
+            .contains("multiple network interfaces are not supported yet"));
+    }
 }

crates/core/src/lib.rs
@@ -4,7 +4,8 @@ mod launcher;
 mod libkrun;
 mod runtime;
 
-pub use config::VmConfig;
+pub use config::{VmConfig, VmNetworkInterfaceConfig};
+pub use vmm_spec::{ResolvedNetworkInterface, VmmLaunchSpec};
 
 #[doc(hidden)]
 pub use runtime::start_vm;