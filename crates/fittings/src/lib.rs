1a112384b0741b8e26a40e4c8a8378511191ed8a
Luiz Ribeiro <luizribeiro@gmail.com>
2026-03-27 04:59:17 -0400

    Workspace bootstrap

---
+++ crates/fittings/src/lib.rs
@@ -0,0 +1,11 @@
+pub const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
+
+#[cfg(test)]
+mod tests {
+    use super::CRATE_NAME;
+
+    #[test]
+    fn smoke_compiles() {
+        assert_eq!(CRATE_NAME, "fittings");
+    }
+}
