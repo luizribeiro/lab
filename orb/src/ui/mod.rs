b80677d04543980988d40b9ee8647f8a9905006a
Luiz Ribeiro <luizribeiro@gmail.com>
2026-05-19 01:28:24 -0400

    Separate transcript storage from replay rendering

--- orb/src/ui/mod.rs
+++ orb/src/ui/mod.rs
@@ -6,6 +6,6 @@ pub mod terminal;
 
 pub use live_viewport::draw;
 pub use scrollback::{
-    CommitColor, commit_blank_line, commit_dim_line, commit_header, commit_markdown,
-    commit_status_line, commit_tool_result, commit_user_prompt, replay_transcript,
+    CommitColor, commit_blank_line, commit_header, commit_markdown, commit_status_line,
+    commit_tool_result, commit_user_prompt, replay_transcript,
 };
